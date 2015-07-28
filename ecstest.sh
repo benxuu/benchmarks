53ace0c8b81945c2dace84d61c5e0e9b33b1a2d3
benxuu <benxuu@163.com>
2015-07-28 23:34:48 +0800

    调整了脚本框架，判断参数

--- ecstest.sh
+++ ecstest.sh
@@ -4,31 +4,38 @@
 #   Author: Ben  
 #   Intro:  
 #===============================================================================================
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
 help()
 {
    cat << HELP
-   This is a generic command line parser demo.
-   USAGE EXAMPLE: ecstest -d ,delete the last data;
+   This is a benchmarks test sheel for aliyun ECS.
+   USAGE EXAMPLE: 
+   ecstest -i ,install the benchmarks;
+   ecstest -d ,delete the last data;
 HELP
    exit 0
 }
-  
+opt=o
+ip="112.124.102.169"  
 while [ -n "$1" ]; do
 case "$1" in
    -h) help;shift 1;; # function help is called
-   -d) opt_d=1;shift 1;; # variable opt_d is set
-   -l) opt_l=$2;shift 2;; # -l takes an argument -> shift by 2
+   -d) opt=d;shift 1;; 
+   -i) opt=i;shift 1;; 
+   -t) opt=t;shift 1;; 
+   -nc) iperfopt=nc;ip=$2;shift 2;;#set this machine as a iperf client,and the server ip is $2
+   -ns) iperfopt=ns;shift 1;;
    --) shift;break;; # end of options
    -*) echo "error: no such option $1. -h for help";exit 1;;
-   *) break;;
+   *) break;; # run all script;
 esac
 done
-if [ $opt_d = 1 ]; then
-   echo "remove some test result file last time"
-   rm /root/*.txt;
-else
-   echo "this is a new test begin!"
-# Update the system
+
+if [ $opt=i ] || [ $opt=o ] ; then
+   echo "install the benchmarks";
+   # Update the system
 apt-get -y update
 apt-get -y upgrade
 #install curl for save file to OSS online;
@@ -40,12 +47,6 @@ apt-get -y install iperf
 apt-get -y install bonnie++
 
 #yum -y install gcc gcc-c autoconf gcc-c++ time perl-Time-HiRes
-
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-hostname=$(hostname)
-export PATH
-
-
 
 # Create new soft download dir
 mkdir -p /opt/unixbench;
@@ -66,10 +67,21 @@ tar -xzf UnixBench5.1.3.tgz;
 cd UnixBench;
 sed -i "s/GRAPHIC_TESTS = defined/#GRAPHIC_TESTS = defined/g" ./Makefile
 make;
+fi
+ 
+
+if [ $opt=d ]; then
+   echo "remove some test result file last time";
+   rm /root/*.txt;
+else
+   echo "this is a new test begin!"
+
 
 #cd $home;
-
+if [ $opt=t ] || [ $opt=o ] ; then
+   echo "start run the benchmarks";
 #Run unixbench
+cd /opt/unixbench;
 echo $(date) >$(hostname).unixbench.txt
 ./Run >>$(hostname).unixbench.txt
 mv $(hostname).unixbench.txt /root
@@ -83,20 +95,23 @@ mkdir /root/bonniedata
 echo $(date) >$(hostname).bonnie.txt
 bonnie++ -d /root/bonniedata/ -u root -s 4096 -m $hostname >>$hostname.bonnie.txt
 curl -T $(hostname).bonnie.txt http://aliyunbenchtest.oss-cn-hangzhou.aliyuncs.com
-
-#Run iperf
-echo $(date) >$(hostname).iperf.txt
-iperf -c 112.124.102.169 >>$(hostname).iperf.txt
-curl -T $(hostname).iperf.txt http://aliyunbenchtest.oss-cn-hangzhou.aliyuncs.com
-
-   
-   
    
 fi
-#echo "opt_f is $opt_f"
-#echo "opt_l is $opt_l"
-#echo "first arg is $1"
-#echo "2nd arg is $2"
+
+#Run iperf
+if [ $iperfopt=ns ] ;then
+echo "set this machine to a iperf server"
+iperf -s;
+fi
+
+if [ $iperfopt=nc ] ;then
+echo $(date) >$(hostname).iperf.txt
+iperf -c $ip >>$(hostname).iperf.txt
+curl -T $(hostname).iperf.txt http://aliyunbenchtest.oss-cn-hangzhou.aliyuncs.com
+fi
+
+
+ 
 
 
 
